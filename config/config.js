ff9ab60aab960ea08af3790f56c51f9e28fa28b2
ThomasKnoops <56110999+ThomasKnoops@users.noreply.github.com>
2024-01-06 23:51:36 +0100

    fix: added config files so professor can run this

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    development: {
+      username: 'admin',
+      password: 'myadmin',
+      database: 'laravel_project',
+      host: "127.0.0.1",
+      port: 3306,
+      dialect: 'mariadb',
+    },
+  };
